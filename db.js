157893e5e9d7f8801c202bca89f49b07daa0f852
semini-jayakodi <jayakodi.sem17@gmail.com>
2024-06-10 01:16:12 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://cluster0:Semini@semini.usbsmxc.mongodb.net/?retryWrites=true&w=majority")
+        const conn = await mongoose.connect("mongodb+srv://Rio:RioAstal1234@rio.kh2t4sq.mongodb.net/?retryWrites=true&w=majority")
         console.log(`MongoDB is connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(`Error ${error}`)
